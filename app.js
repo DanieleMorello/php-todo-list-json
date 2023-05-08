e5ad353e55e2eb58e5ef5b70e266e5a19433ebea
DanieleMorello <danielemorello@live.it>
2023-05-08 17:02:30 +0200

    Add the logic to print the new task to page

--- app.js
+++ app.js
@@ -4,9 +4,28 @@ createApp({
     return {
       tasks: null,
       api_url: "get_tasks.php",
+      new_task: "",
     };
   },
-  methods: {},
+  methods: {
+    add_task() {
+      console.log("Aggiungi task");
+      const data = {
+        new_task: this.new_task,
+      };
+      axios
+        .post("store_task.php", data, {
+          headers: { "content-Type": "multipart/form-data" },
+        })
+        .then((response) => {
+          console.log(response);
+          this.tasks = response.data;
+        })
+        .catch((error) => {
+          console.error();
+        });
+    },
+  },
   mounted() {
     axios
       .get(this.api_url)
---
+++ store_task.php
@@ -0,0 +1,29 @@
+<?php
+
+// var_dump($_POST);
+if (isset($_POST['new_task'])) {
+  $task = $_POST['new_task'];
+
+  // Legge il file json con file_get_contents
+  $tasks_string = file_get_contents('tasks.json');
+
+  // Converte la stringa json in un array associativo con json_decode() 
+  $tasks_array = json_decode($tasks_string, true);
+  // var_dump($tasks_array);
+
+  // Aggiunge la nuova task all' array
+  array_push($tasks_array, $task);
+  // var_dump($tasks_array);
+
+  // Converte l' array in json
+  $new_tasks_json_string = json_encode($tasks_array);
+
+  // Sostituisce il contenuto del file usando file_put_contents()
+  file_put_contents('tasks.json', $new_tasks_json_string);
+
+  // Aggiunge header application/json
+  header('Content-Type: application/json');
+
+  // Stampa il json
+  echo $new_tasks_json_string;
+}
